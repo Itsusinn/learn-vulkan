e4e96b176216a1a616bca86470cdf186f32cc053
Itsusinn <itsusinn@meowcat.org>
2022-07-10 16:55:47 +0800

    style: cargo fmt

--- src/log.rs
+++ src/log.rs
@@ -3,24 +3,24 @@ use tracing::Level;
 use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 
 pub(crate) fn init() {
-   tracing_subscriber::registry()
-      .with(
-         tracing_subscriber::fmt::layer()
-            .with_target(true)
-            .with_timer(tracing_subscriber::fmt::time::OffsetTime::new(
-               time::UtcOffset::from_whole_seconds(
-                  Local.timestamp(0, 0).offset().fix().local_minus_utc(),
-               )
-               .unwrap_or(time::UtcOffset::UTC),
-               time::macros::format_description!(
-                  "[year repr:last_two]-[month]-[day] [hour]:[minute]:[second]"
-               ),
-            )),
-      )
-      .with(
-         tracing_subscriber::filter::Targets::new()
-            .with_target("voxel", Level::TRACE)
-            .with_default(Level::WARN),
-      )
-      .init();
+  tracing_subscriber::registry()
+    .with(
+      tracing_subscriber::fmt::layer()
+        .with_target(true)
+        .with_timer(tracing_subscriber::fmt::time::OffsetTime::new(
+          time::UtcOffset::from_whole_seconds(
+            Local.timestamp(0, 0).offset().fix().local_minus_utc(),
+          )
+          .unwrap_or(time::UtcOffset::UTC),
+          time::macros::format_description!(
+            "[year repr:last_two]-[month]-[day] [hour]:[minute]:[second]"
+          ),
+        )),
+    )
+    .with(
+      tracing_subscriber::filter::Targets::new()
+        .with_target("voxel", Level::TRACE)
+        .with_default(Level::WARN),
+    )
+    .init();
 }
